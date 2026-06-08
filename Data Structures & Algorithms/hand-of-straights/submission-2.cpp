97013451314c80f6617d0fe9ec7d5f560607173e
Akhil Dhyani <89084510+aphrodoe@users.noreply.github.com>
2026-06-08 23:23:26 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        map<int, int> mp;
+        int n = hand.size();
+        for (int i = 0; i < n; i++) mp[hand[i]]++;
+        if (n % groupSize != 0) return false;
+        int groups = n / groupSize;
+        for (int i = 0; i < groups; i++) {
+            int curr = mp.begin()->first;
+            if (mp[curr] == 1) mp.erase(curr);
+            else mp[curr]--;
+            for (int i = 1; i < groupSize; i++) {
+                if (mp.find(curr + 1) == mp.end()) return false;
+                else {
+                    curr++;
+                    if (mp[curr] == 1) mp.erase(curr);
+                    else mp[curr]--;
+                }
+            }
+        }
+        return true;
+    }
+};
