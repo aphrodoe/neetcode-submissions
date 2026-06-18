3cdd648017caa6e8ca95ec28646918805333140c
Akhil Dhyani <89084510+aphrodoe@users.noreply.github.com>
2026-06-18 16:06:02 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode* curr = root;
+
+        while (curr != NULL) {
+            if (curr -> left == NULL) {
+                inorder.push_back(curr -> val);
+                curr = curr -> right;
+            }
+            else {
+                TreeNode* temp = curr -> left;
+                while (temp -> right != NULL && temp -> right != curr) temp = temp -> right;
+                if (temp -> right == NULL) {
+                    temp -> right = curr;
+                    curr = curr -> left;
+                }
+                else {
+                    temp -> right = NULL;
+                    inorder.push_back(curr -> val);
+                    curr = curr -> right;
+                }
+            }
+        }
+
+        for (int i = 1; i < inorder.size(); i++) {
+            if (inorder[i] <= inorder[i - 1]) return false;
+        }
+
+        return true;
+    }
+};
