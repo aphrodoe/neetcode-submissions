f7cfd98ae189c284c0e3d51b1c40b7708738449e
Akhil Dhyani <89084510+aphrodoe@users.noreply.github.com>
2026-06-22 15:55:48 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int ways(int n, vector<int>& dp) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (dp[n] != -1) return dp[n];
+
+        return dp[n] = ways(n - 1, dp) + ways(n - 2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return ways(n, dp);
+    }
+};
