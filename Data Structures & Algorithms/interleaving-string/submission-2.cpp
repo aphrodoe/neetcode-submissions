21b74156c0156a89d5448656aa27394785a0bc18
Akhil Dhyani <89084510+aphrodoe@users.noreply.github.com>
2026-06-28 02:37:29 +0530

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool check(int i, int j, int k, string& s1, string& s2, string& s3) {
+        if (i < 0 && j < 0 && k < 0) return true;
+        if (s1[i] == s3[k] && s2[j] == s3[k]) {
+            bool first = check(i - 1, j, k - 1, s1, s2, s3);
+            bool second = check(i, j - 1, k - 1, s1, s2, s3);
+            return (first | second);
+        }
+        else if (s1[i] == s3[k]) return check(i - 1, j, k - 1, s1, s2, s3);
+        else if (s2[j] == s3[k]) return check(i, j - 1, k - 1, s1, s2, s3);
+        else return false;
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.size(), n2 = s2.size(), n3 = s3.size();
+        if (n1 + n2 != n3) return false;
+        return check(n1 - 1, n2 - 1, n3 - 1, s1, s2, s3);
+    }
+};
