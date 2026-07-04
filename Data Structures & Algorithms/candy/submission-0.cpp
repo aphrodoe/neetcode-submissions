8a46e7b6a826b8bebc5af10e98465229049e24d2
Akhil Dhyani <89084510+aphrodoe@users.noreply.github.com>
2026-07-04 19:56:34 +0530

    Add: candy - submission-0

---
+++ Data Structures & Algorithms/candy/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n);
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+            else left[i] = 1;
+        }
+        int curr = 1;
+        int candy = max(curr, left[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) curr++;
+            else curr = 1;
+            candy += max(curr, left[i]);
+        }
+        return candy;
+    }
+};
