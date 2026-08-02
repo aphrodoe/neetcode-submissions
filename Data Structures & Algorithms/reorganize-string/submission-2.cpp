085d7ff2b1ae48eacda22e9659a5cdce2b8b6aa2
Akhil Dhyani <89084510+aphrodoe@users.noreply.github.com>
2026-08-02 14:31:46 +0530

    Add: reorganize-string - submission-2

---
+++ Data Structures & Algorithms/reorganize-string/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        priority_queue<pair<int, char>> pq;
+        queue<pair<pair<int, char>, int>> q;
+        unordered_map<char, int> mp;
+        for (char& c : s) mp[c]++;
+        for (auto& it : mp) pq.push({it.second, it.first});
+
+        string ans;
+        int pos = 0;
+        while (!pq.empty() || !q.empty()) {
+            while (!q.empty() && pos > q.front().second) {
+                auto it = q.front();
+                q.pop();
+                pq.push({it.first.first, it.first.second});
+            }
+            if (pq.empty()) break;
+            auto it = pq.top();
+            pq.pop();
+            ans.push_back(it.second);
+            int cnt = it.first - 1;
+            if (cnt > 0) q.push({{cnt, it.second}, pos + 1});
+            pos++;
+        }
+
+        if (ans.size() != s.size()) return "";
+        return ans;
+    }
+};
